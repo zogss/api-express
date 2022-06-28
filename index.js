0ddaf347c3164fe62e1ac7cafa4b80a4cee30190
zogss <yanlucasbranco@gmail.com>
2022-06-28 20:46:59 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// configuração inicial
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+require('dotenv').config();
+
+// forma de ler JSON / middlewares
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+
+app.use(express.json());
+
+// rotas da API
+const PersonRoutes = require('./routes/PersonRoutes')
+
+app.use('/person', PersonRoutes)
+
+// rota inicial / endpoint
+app.get('/', (req, res)=>{
+
+    // mostrar REQsição
+    res.json({ message: 'Oi express!' })
+});
+
+// entregar uma porta ao express para ter acesso ao codigo
+const DB_USER = 'matheus'
+const DB_PASSWORD = encodeURIComponent('FsMOMhMpVk9Al9UE') 
+
+mongoose
+    .connect(process.env.DB_LINK)
+    .then(()=>{
+        console.log('Conectamos ao MongoDB!')
+        app.listen(3000)
+    })
+    .catch((err) => console.log(err))
